6bd1d8dc99e00c967fd732c89e6dc7a505788801
lookuptrendzofficial <lookuptrendzofficial@gmail.com>
2026-07-10 16:59:29 +0530

    Add files via upload

---
+++ App/page.tsx
@@ -0,0 +1,16 @@
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Stats from "../components/Stats";
+import About from "../components/About";
+
+export default function Home() {
+  return (
+      <main>
+            <Header />
+                  <Hero />
+                        <Stats />
+                              <About />
+                                  </main>
+                                    );
+                                    }
+                                    
